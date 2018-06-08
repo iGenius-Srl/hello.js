8cfbfe92f3cb516a73cf52108fbebf12bd1091c4
Shy <shy@igenius.net>
2018-06-08 14:21:18 +0200

    fix the minify

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,46 +1,42 @@
 module.exports = function config(grunt) {
-    grunt.initConfig({
-        pkg: grunt.file.readJSON('package.json'),
-        shunt: {
-            build: {
-                'dist/hello.js': [
-                    'src/hello.polyfill.js',
-                    'src/hello.js',
-                    'src/hello.amd.js',
-                    'src/hello.commonjs.js'
-                ],
-                'dist/hello.all.js': [
-                    'src/hello.polyfill.js',
-                    'src/hello.js',
-                    'src/modules/dropbox.js',
-                    'src/modules/facebook.js',
-                    'src/modules/flickr.js',
-                    'src/modules/foursquare.js',
-                    'src/modules/github.js',
-                    'src/modules/google.js',
-                    'src/modules/instagram.js',
-                    'src/modules/joinme.js',
-                    'src/modules/linkedin.js',
-                    'src/modules/soundcloud.js',
-                    'src/modules/twitter.js',
-                    'src/modules/vk.js',
-                    'src/modules/youtube.js',
-                    'src/modules/gAnalytics.js',
-                    'src/modules/windows.js',
-                    'src/modules/yahoo.js',
-                    'src/hello.amd.js',
-                    'src/hello.commonjs.js'
-                ]
-            },
-            minify: {
-                'dist/hello.min.js': 'dist/hello.js',
-                'dist/hello.all.min.js': 'dist/hello.all.js'
-            }
-        },
-    });
+	grunt.initConfig({
+		pkg: grunt.file.readJSON("package.json"),
+		shunt: {
+			build: {
+				"dist/hello.js": [
+					"src/hello.polyfill.js",
+					"src/hello.js",
+					"src/hello.amd.js",
+					"src/hello.commonjs.js"
+				],
+				"dist/hello.all.js": [
+					"src/hello.polyfill.js",
+					"src/hello.js",
+					"src/modules/facebook.js",
+					"src/modules/google.js",
+					"src/modules/instagram.js",
+					"src/modules/linkedin.js",
+					"src/modules/twitter.js",
+					"src/modules/youtube.js",
+					"src/modules/gAnalytics.js",
+					"src/hello.amd.js",
+					"src/hello.commonjs.js"
+				]
+			}
+		},
+		uglify: {
+			minify: {
+				files: {
+					"dist/hello.min.js": ["dist/hello.js"],
+					"dist/hello.all.min.js": ["dist/hello.all.js"]
+				}
+			}
+		}
+	});
 
-    grunt.loadNpmTasks('shunt');
+	grunt.loadNpmTasks("grunt-contrib-uglify");
 
-    grunt.registerTask('default', ['shunt:build', 'shunt:minify']);
+	grunt.loadNpmTasks("shunt");
 
+	grunt.registerTask("default", ["shunt:build", "uglify:minify"]);
 };
