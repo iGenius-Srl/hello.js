3b56211a385eb0ccd304b5ffba78517384f9d093
Luca Galeazzi <luca.galeazzi@igenius.ai>
2020-01-09 09:11:47 +0100

    forgot to build

--- dist/hello.all.js
+++ dist/hello.all.js
@@ -2793,7 +2793,7 @@ hello.utils.extend(hello.utils, {
 hello.utils.responseHandler(window, window.opener || window.parent);
 
 (function(hello) {
-	var version = "3.3";
+	var version = "4.0";
 	hello.init({
 		facebook: {
 			name: "Facebook",
